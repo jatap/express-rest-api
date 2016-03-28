6a23d28195fea93ca108f3d5666591df6d9ebaa6
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2016-03-29 00:26:10 +0100

    Add CORS support. TG-9 #closed. TG-12 #done.

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+// CORS Support
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Logger
 app.use(logger('dev'));
 
